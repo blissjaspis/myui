4c07cdcb03d3749e4210ba0cb9635cb7e08a05c0
Bliss Jaspis <jaspis_zzt@ymail.com>
2026-02-24 09:02:28 +0700

    Refactor Sheet component styles: simplify button class definition in content view and add right padding to header for improved layout consistency.

--- resources/views/components/sheet/header.blade.php
+++ resources/views/components/sheet/header.blade.php
@@ -3,6 +3,6 @@
     A container for the header section of a sheet with title and description.
     Documentation: docs/components/sheet.md
 --}}
-<div {{ $attributes->merge(['class' => 'flex flex-col space-y-1.5 text-left']) }}>
+<div {{ $attributes->merge(['class' => 'flex flex-col space-y-1.5 text-left pr-10']) }}>
     {{ $slot }}
 </div>
