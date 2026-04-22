89676b930913cb085442ccaf447fc9e875a9e4b0
Bliss Jaspis <jaspis_zzt@ymail.com>
2026-04-22 11:46:51 +0700

    Update flag icon SVG path for improved design and clarity

--- resources/views/components/icons/flag.blade.php
+++ resources/views/components/icons/flag.blade.php
@@ -1,3 +1,3 @@
 <svg {{ $attributes->merge() }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" d="m15.75 15.75-2.489-2.489m0 0a3.375 3.375 0 1 0-4.773-4.773 3.375 3.375 0 0 0 4.774 4.774ZM21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5" />
 </svg>
